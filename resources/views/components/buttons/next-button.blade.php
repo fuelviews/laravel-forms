f7ae74fac5fdc6c84cac3f7a7fd82a835defed0e
thejmitchener <thejmitchener@gmail.com>
2025-09-27 13:59:40 -0400

    Update button color classes for consistency and improve dark mode support in the thank-you component.

--- resources/views/components/buttons/next-button.blade.php
+++ resources/views/components/buttons/next-button.blade.php
@@ -1,4 +1,4 @@
 <button wire:click="nextStep"
-        class="bg-red-500 hover:bg-red-700 text-white text-md font-bold py-2 px-4 rounded-lg">
+        class="bg-prime-500 hover:bg-prime-700 text-white text-md font-bold py-2 px-4 rounded-lg">
     {{ $buttonText }}
 </button>
